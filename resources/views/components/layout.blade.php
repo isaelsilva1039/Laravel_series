159d300b05a211824fb13674962831cf2281e728
isael <isael.silva@evolutto.com.br>
2022-05-14 18:18:26 -0300

    banco ,

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,14 +6,27 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-{{--    <link rel="stylesheet" href="{{ 'app/css.css'  }}" >--}}
+    {{--    <link rel="stylesheet" href="{{ 'app/css.css'  }}" >--}}
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <title>{{ $title }}</title>
 </head>
 <body>
+<div class="pos-f-t">
+    <div class="collapse" id="navbarToggleExternalContent">
+        <div class="bg-dark p-4">
+            <h4 class="text-white">Collapsed content</h4>
+            <span class="text-muted">Toggleable via the navbar brand.</span>
+        </div>
+    </div>
+    <nav class="navbar navbar-dark bg-dark">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </nav>
+</div>
 <div class="container">
     <h1>{{$title}}</h1>
     {{ $slot }}
 </div>
-    </body>
-    </html>
+</body>
+</html>
